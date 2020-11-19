8d805b701ed99a6f915be25c4a526b81fd32cabe
Keith Billings <heyitsbo91@gmail.com>
2020-11-19 00:40:57 -0800

    add input box with onchange event

--- src/App.js
+++ src/App.js
@@ -1,46 +1,62 @@
-// import { Component } from "react";
-import { useEffect, useState } from 'react';
+import { Component } from "react";
+// import { useEffect, useState } from 'react';
 import "./App.css";
 import CardList from "./components/card-list/card-list"; // functional component import
 
-// class App extends Component {
-//   constructor() {
-//     super();
+class App extends Component {
+  constructor() {
+    super();
 
-//     this.state = {
-//       monsters: [],
-//     };
-//   }
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
 
-//   componentDidMount() {
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <input
+          type="search"
+          placeholder="Search for Monsters"
+          onChange={(e) => this.setState({ searchField: e.target.value })}
+        />
+        <CardList monsters={this.state.monsters} />
+      </div>
+    );
+  }
+}
+
+// function App() {
+//   const [users, setUsers] = useState([]);
+//   const [searchField, setSearchField] = useState("");
+
+//   useEffect(() => {
 //     fetch("https://jsonplaceholder.typicode.com/users")
 //       .then(res => res.json())
-//       .then(users => this.setState({ monsters: users }));
-//   }
+//       .then(users => setUsers(users))
+//   }, []);
 
-//   render() {
-//     return (
-//       <div className="App">
-//         <CardList monsters={this.state.monsters} />
-//       </div>
-//     );
-//   }
+//   useEffect(() => {
+//     console.log(searchField)
+//   }, [searchField]);
+
+//   return (
+//     <div className="App">
+//       <input
+//         type="search"
+//         placeholder="Search for Monsters"
+//         onChange={(e) => setSearchField(e.target.value)}
+//       />
+//       <CardList monsters={users} />
+//     </div>
+//   )
 // }
-
-function App() {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then(res => res.json())
-      .then(users => setUsers(users))
-  }, []);
-
-  return (
-    <div className="App">
-      <CardList monsters={users} />
-    </div>
-  )
-}
 
 export default App;
